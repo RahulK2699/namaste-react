0f5fc0f8e0f5431440c1dada673a23780e1d83b9
RahulK2699 <rahul.kharche@tricog.com>
2024-01-24 21:24:29 +0530

    Igniting our app episode 2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
 const div = React.createElement('div', { id: 'heading' }, [
   React.createElement('div', { id: 'parent' }, [
     React.createElement('h1', {}, 'I am H1 tag'),
-    React.createElement('h2', {}, 'I am H2 tag')
+    React.createElement('h2', {}, 'sycwufweyfygr ygweyew ewr gr')
   ]),
   React.createElement('div', { id: 'children' }, [
     React.createElement('h1', {}, 'I am H1 tag'),
@@ -9,6 +12,6 @@ const div = React.createElement('div', { id: 'heading' }, [
   ])
 ])
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 
 root.render(div)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# parcel
+- Dev build
+- Local server
+- HMR: Hot Module Replacement 
+- File Watching Algorithm - written in C++
+- Caching- Faster Builds
+- Image Optimization
+- Manification (in production build)
+- Bundling
+- Compressing 
+- Consistent Hashing
+- Code Splitting
+- Differentail Bundling - support older browsers
+- Diagnostic
+- Error handling
+- HTTP's
+- Tree Shaking - Remove Unsused code
+- Differnet dev and prod bundles
