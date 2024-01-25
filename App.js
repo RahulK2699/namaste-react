4a1ceb98f53c8481b4a96d60a1b7e68db79abcb4
RahulK2699 <rahul.kharche@tricog.com>
2024-01-25 12:55:37 +0530

    laying the foundation episode-3

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-const div = React.createElement('div', { id: 'heading' }, [
-  React.createElement('div', { id: 'parent' }, [
-    React.createElement('h1', {}, 'I am H1 tag'),
-    React.createElement('h2', {}, 'sycwufweyfygr ygweyew ewr gr')
-  ]),
-  React.createElement('div', { id: 'children' }, [
-    React.createElement('h1', {}, 'I am H1 tag'),
-    React.createElement('h2', {}, 'I am H2 tag')
-  ])
-])
+const heading = React.createElement('h1', { id: 'heading' }, 'Namaste JS')
+
+//React Functional Component
+
+const Title = () => {
+  return <h1 className='title'>Namaste React Title</h1>
+}
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h1 id='heading'>Namaste react Functional Component</h1>
+    </div>
+  )
+}
 
 const root = createRoot(document.getElementById('root'))
 
-root.render(div)
+root.render(<HeadingComponent />)
